d812e110dd68ad7381b23a7e0864094d6334fb10
ajaynew <asingala@hotmail.com>
2019-01-24 18:23:27 +0530

    Chrome exe removed. New test added.

---
+++ SeleniumTestSample/SeleniumTestSample/UnitTest2.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestSample
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestFoo()
+        {
+            // Arrange
+            var name = "Ajay Singala";
+            var expected = "AJAY";
+
+            // Act
+            var result = name.Substring(0, 4).ToUpper();
+
+            // Assert
+            Assert.AreEqual<string>(expected, result);
+        }
+    }
+}
